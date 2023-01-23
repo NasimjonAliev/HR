8236e2d483f6dca7f8998c600bdd6d626c2e4f4f
NasimjonAliev <114052589+NasimjonAliev@users.noreply.github.com>
2023-01-23 14:08:04 +0500

    Отмена изменений

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -5,7 +5,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string FatherName { get; set; }
-        public int  Age_Person { get; set; }
+        public int  Age { get; set; }
         public string PhoneNumber { get; set; }
         public string PassportNumber { get; set; }
     }
