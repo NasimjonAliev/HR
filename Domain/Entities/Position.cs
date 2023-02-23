52401a5fe0d7df186f4ab84ca38b7a4cff610eb8
NasimjonAliev <114052589+NasimjonAliev@users.noreply.github.com>
2023-02-23 13:48:25 +0500

    new

--- Domain/Entities/Position.cs
+++ Domain/Entities/Position.cs
@@ -7,7 +7,6 @@ namespace Domain.Entities
     {
         public string Name { get; set; }
         public double Amount { get; set; }
-
         public ICollection<Staff> Staffs { get; set; } = new Collection<Staff>();
     }
 }
